0d66c873c2bf4a2e9c906dd9724a190b43e3945f
Baga <magnushasrp@gmail.com>
2020-09-07 11:09:11 +0300

    Added task3 package

---
+++ src/com/mirea/work_3/TestShapes.java
@@ -0,0 +1,31 @@
+package com.mirea.work_3;
+
+public class TestShapes {
+
+    public static void main(String[] args) {
+        Shape s1 = new Circle(5.5, "RED", false); // Upcast Circle to Shape
+        System.out.println(s1);
+        System.out.println(s1.getArea());
+        System.out.println(s1.getPerimeter());
+        System.out.println(s1.getColor());
+        System.out.println(s1.isFilled());
+        System.out.println(((Circle) s1).getRadius());
+
+
+        Circle c1 = (Circle) s1;
+        System.out.println(c1);
+        System.out.println(c1.getArea());
+        System.out.println(c1.getPerimeter());
+        System.out.println(c1.getColor());
+        System.out.println(c1.isFilled());
+        System.out.println(c1.getRadius());
+
+        Shape s3 = new Rectangle(1.0, 2.0, "RED", false);
+        System.out.println(s3);
+        System.out.println(s3.getArea());
+        System.out.println(s3.getPerimeter());
+        System.out.println(s3.getColor());
+        System.out.println(((Rectangle) s3).getLength());
+
+    }
+}
