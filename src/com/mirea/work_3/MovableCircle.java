a67f317c7cb5701bd8adb752a592dc5dc3271352
Baga <magnushasrp@gmail.com>
2020-09-07 11:31:53 +0300

    task3 added interface

---
+++ src/com/mirea/work_3/MovableCircle.java
@@ -0,0 +1,37 @@
+package com.mirea.work_3;
+
+public class MovableCircle implements Movable{
+    private int radius;
+    private MovablePoint center;
+
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
+        center = new MovablePoint(x, y, xSpeed, ySpeed);
+        this.radius = radius;
+    }
+
+    @Override
+    public void moveUp() {
+        center.moveUp();
+    }
+
+    @Override
+    public void moveDown() {
+        center.moveDown();
+    }
+
+    @Override
+    public void moveLeft() {
+        center.moveLeft();
+    }
+
+    @Override
+    public void moveRight() {
+        center.moveRight();
+    }
+
+    @Override
+    public String toString() {
+        return "Circle at (" + center.x + ", " + center.y + "), radius = " + this.radius;
+
+    }
+}
