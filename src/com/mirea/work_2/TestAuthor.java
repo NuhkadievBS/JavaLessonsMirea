3dfb512792fdebe38fe623a3574863964a092422
Baga <magnushasrp@gmail.com>
2020-09-07 10:35:59 +0300

    Added task2 package

---
+++ src/com/mirea/work_2/TestAuthor.java
@@ -0,0 +1,12 @@
+package com.mirea.work_2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author author1 = new Author("John", "ogurec1996@mail.ru", 'm');
+        Author author2 = new Author("Emily", "after13@mail.ru", 'f');
+        Author author3 = new Author("Svyatozar", "loktar_ogar@mail.ru", 'm');
+        System.out.println(author1);
+        System.out.println(author2);
+        System.out.println(author3);
+    }
+}
